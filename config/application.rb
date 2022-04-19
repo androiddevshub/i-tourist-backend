cde107abb440da0b11a0041229ef3a5300859c67
Shubham jain <beardo@192.168.1.4>
2022-04-19 12:58:45 +0530

    Added local env file

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,12 @@ module ITouristBackend
     config.load_defaults 6.1
     config.paths.add File.join("app", "api"), glob: File.join("**", "*.rb")
     config.autoload_paths += Dir[Rails.root.join("app", "api", "*")]
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
